ec91091e2abe7f0e31a10ba25bffbee916fa8ecd
Md Thamedul Islam <sumonmti498@gmail.com>
2023-07-12 03:55:05 +0600

    showuser

--- resources/views/showuser.blade.php
+++ resources/views/showuser.blade.php
@@ -5,10 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Show User</title>
+    <style>
+        table {
+          font-family: arial, sans-serif;
+          border-collapse: collapse;
+          width: 50%;
+        }
+        
+        td, th {
+          border: 1px solid #dddddd;
+          text-align: left;
+          padding: 8px;
+        }
+        
+        tr:nth-child(even) {
+          background-color: #dddddd;
+        }
+        </style>
 </head>
 <body>
     
-    <table border="border: 1px solid black;">
+    <table>
         <tr>
             <th>SL</th>
             <th>Name</th>
